16683b8d70422b33c4c0a6c195d803cb769c9b1c
Fox-or-Cat <yekaterina.chernyakova@gmail.com>
2023-01-24 23:16:03 +0300

    next cata

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println(IsTriangle(6, 7, 8))
+	fmt.Println(ToJadenCase("How can mirrors be real if our eyes aren't real"))
 }
+func ToJadenCase(str string) string {
+	// arr := strings.Split(str, " ")
+	// fmt.Println(arr)
+	result := ""
+	result += strings.ToUpper(string(str[0]))
+	for i := 1; i < len(str); i++ {
 
-func IsTriangle(a, b, c int) bool {
-	if a+b > c && a+c > b && b+c > a {
-		return true
+		if string(str[i-1]) == " " {
+			result += strings.ToUpper(string(str[i]))
+		} else {
+			result += string(str[i])
+		}
 	}
-	return false
+	// arr1 := strings.ToUpper(arr[0])
+	// fmt.Println(arr1)
+	return result
 }
