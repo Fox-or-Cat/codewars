123dd2bd9bc50734b60bf35698b8473d89506fe1
Fox-or-Cat <yekaterina.chernyakova@gmail.com>
2023-01-25 16:53:07 +0300

    new Kata

--- main.go
+++ main.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"strconv"
 )
 
 func main() {
-	fmt.Println(MaxRot(56789))
+	fmt.Println(Calc("ifkhchlhfd"))
 }
-func MaxRot(n int64) int64 {
-	str := strconv.FormatInt(n, 10)
-	max := n
-	for i := 0; i < len(str)-1; i++ {
-		str = str[:i] + str[i+1:] + string(str[i])
-		current, _ := strconv.ParseInt(str, 10, 64)
-		if current > max {
-			max = current
+func Calc(s string) int {
+	runeArray := []rune(s)
+	str1 := ""
+	for i := 0; i < len(runeArray); i++ {
+		str1 += strconv.FormatInt(int64(runeArray[i]), 10)
+	}
+	str2 := ""
+	for i := 0; i < len(str1); i++ {
+		if string(str1[i]) == "7" {
+			str2 += "1"
+		} else {
+			str2 += string(str1[i])
 		}
 	}
-	return max
+	total1, _ := new(big.Int).SetString(str1, 10)
+	total2, _ := new(big.Int).SetString(str2, 10)
+	diff := new(big.Int)
+	diff.Sub(total1, total2)
+	diffStr := diff.String()
+	result := 0
+	for i := 0; i < len(diffStr); i++ {
+		num, _ := strconv.Atoi(string(diffStr[i]))
+		result += num
+	}
+	return result
 }
